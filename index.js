10dcf0c94ddb5a2e499c81eb0cdf4d2086afacef
Rajan <rajanvasani9@gmail.com>
2025-01-02 14:25:17 +0530

    updated code

--- index.js
+++ index.js
@@ -96,15 +96,15 @@ const io = new Server(server, {
 app.use(bodyParser.json());
 app.set("trust proxy", 1);
 app.use(express.json({ limit: "4mb" }));
-// app.use(cors());
-app.use(
-	cors({
-		origin: '*',
-		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-		allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-		credentials: true
-	})
-);
+app.use(cors());
+// app.use(
+// 	cors({
+// 		origin: '*',
+// 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// 		allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+// 		credentials: true
+// 	})
+// );
 
 
 
